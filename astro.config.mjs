6bdc7dcb725bcf97ee1deb88119bc660faa87dc2
threegoeseasy <threegoeseasy@gmail.com>
2023-08-26 09:41:48 +0300

    mod deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name]-[hash].js",
+          assetFileNames: "assets/[name]-[hash][extname]",
+        },
+      },
+    },
+  },
+  site: "https://threegoeseasy.ru/",
+  output: "static",
   integrations: [
     preact(),
     tailwind(),
@@ -13,4 +26,7 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
